ed702156ee9e5e697e35ca694c9e6f30c3868532
amee13bit <ameerali367@gmail.com>
2017-08-20 12:45:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,8 +5,8 @@ var img = document .getElementById("madi");
 var marginLeft = 0;
 function moveRight(){
     marginLeft = marginLeft + 10;
-    img.style.marginLeft = marginLeft+ 'px' ;
-}
+    img.style.marginLeft = marginLeft + 'px' ;
+} 
 img.onclick = function() {
     var interval = setinterval(moveRight, 100);
 };
