23a283ef64456117da2ce88d49b189a1b001c6c4
amee13bit <ameerali367@gmail.com>
2017-08-20 12:30:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,7 @@
-console.log('Loaded!');
+console.log('Loaded!');;
 var element = document.getElementById("main.text");
 element.innerHTML = "newvalue";
+var img = document .getElementById("madi");
+img.onclick = function() {
+    img.style.marginleft = '100px';
+}
