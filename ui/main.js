405d935730467ba3191392116a1475e6ee7d3cfa
amee13bit <ameerali367@gmail.com>
2017-08-20 12:44:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,11 @@
-console.log('Loaded!');;
+console.log('Loaded!');
 var element = document.getElementById("main.text");
 element.innerHTML = "newvalue";
 var img = document .getElementById("madi");
 var marginLeft = 0;
 function moveRight(){
     marginLeft = marginLeft + 10;
-    img.style.marginLeft = marginLeft+ 'px' 
+    img.style.marginLeft = marginLeft+ 'px' ;
 }
 img.onclick = function() {
     var interval = setinterval(moveRight, 100);
